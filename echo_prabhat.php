9d97e0188feed8ea69cfc6d20a40db3ccd66e0d9
Prabhat Kumar <prabhat@Prabhats-MacBook-Air.local>
2023-10-04 19:54:37 -0700

    initial commit

---
+++ echo_prabhat.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h2>PHP is Fun, This is Prabhat site!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP! Hurray!!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?>
