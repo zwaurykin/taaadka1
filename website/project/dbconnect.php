01d205e6b507dca31671208d27b77965d426c6ea
zwaurykin <prabhat.kumar0220@gmail.com>
2023-11-15 22:19:03 -0800

    dbsm

--- website/project/dbconnect.php
+++ website/project/dbconnect.php
@@ -3,5 +3,4 @@ $servername = "db5012723904.hosting-data.io";
 $username = "dbu1425408";
 $password = "CMPE@272SJSU";
 $dbname = "dbs10688165";
-$conn = new mysqli($servername, $username, $password, $dbname);
-?>
+$conn = new mysqli($servername, $username, $password, $dbname, 3306);
--- website/project/products/dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "db5012723904.hosting-data.io";
-$username = "dbu1425408";
-$password = "CMPE@272SJSU";
-$dbname = "dbs10688165";
-$conn = new mysqli($servername, $username, $password, $dbname,3306);
-?>
