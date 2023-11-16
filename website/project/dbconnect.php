1c77f3599e9b787fc5dd53f32e830e882584d5b9
zwaurykin <prabhat.kumar0220@gmail.com>
2023-11-15 22:23:14 -0800

    dbsmm

--- website/project/dbconnect.php
+++ website/project/dbconnect.php
@@ -1,6 +1,7 @@
 <?php
 $servername = "db5012723904.hosting-data.io";
 $username = "dbu1425408";
-$password = "CMPE@272SJSU";
+$password = "cmpe272user";
 $dbname = "dbs10688165";
-$conn = new mysqli($servername, $username, $password, $dbname, 3306);
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
