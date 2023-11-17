7b9bc0473ce4029fa8d57697755abe1c196fc185
zwaurykin <prabhat.kumar0220@gmail.com>
2023-11-16 19:34:37 -0800

    added db

--- website/project/dbconnect.php
+++ website/project/dbconnect.php
@@ -1,7 +1,6 @@
 <?php
-$servername = "db5012723904.hosting-data.io";
-$username = "dbu1425408";
-$password = "cmpe272user";
-$dbname = "dbs10688165";
-$conn = new mysqli($servername, $username, $password, $dbname);
+$servername = "db272.cv99a47vgyml.us-west-1.rds.amazonaws.com";
+$username = "admin";
+$password = "password";
+$dbname = "cmpe272";
 ?>
